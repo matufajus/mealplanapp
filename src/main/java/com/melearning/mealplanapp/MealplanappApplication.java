bde19f6b177c806f468c9ebc4f3a743ae183f761
matufajus <matas.tas@gmail.com>
2019-12-02 14:15:40 +0200

    Spring Boot initaliser

---
+++ src/main/java/com/melearning/mealplanapp/MealplanappApplication.java
@@ -0,0 +1,13 @@
+package com.melearning.mealplanapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MealplanappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MealplanappApplication.class, args);
+	}
+
+}
