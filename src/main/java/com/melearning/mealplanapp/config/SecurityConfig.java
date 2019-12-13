6bee04aec14ed2e8b493638b4ad8900433f9726c
matufajus <matas.tas@gmail.com>
2019-12-13 11:42:11 +0200

    spring security jdbc authentication

--- src/main/java/com/melearning/mealplanapp/config/SecurityConfig.java
+++ src/main/java/com/melearning/mealplanapp/config/SecurityConfig.java
@@ -1,5 +1,8 @@
 package com.melearning.mealplanapp.config;
 
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
@@ -12,6 +15,9 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+	
+	@Autowired
+	DataSource dataSource;
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
@@ -31,10 +37,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.inMemoryAuthentication()
-        	.withUser("user").password("{noop}user123").roles("USER");
-    	auth.inMemoryAuthentication()
-        	.withUser("admin").password("{noop}admin123").roles("USER", "ADMIN");
+        auth.jdbcAuthentication().dataSource(dataSource);
     }
     
 }
